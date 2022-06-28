fc50cf711b0b3f727b4529cb41bd1f1ab9a6b132
晨风 <cezres@163.com>
2022-06-28 16:14:38 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CloudKitWebServices",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
         .macOS(.v11)
     ],
     products: [
